8e87dfce50e6e229a4449ae8cfdbba08ca35ea6c
Dung Nguyen <thienhoangphuongdung@gmail.com>
2025-12-05 16:48:41 +0100

    refactor: remake projet structure

--- parent_pi/app/routes.py
+++ parent_pi/app/routes.py
@@ -1,14 +1,21 @@
-import os
 from pathlib import Path
-from flask import Flask, request, render_template, jsonify, send_from_directory
+from flask import Blueprint, request, render_template, jsonify, send_from_directory
 
-app = Flask(__name__)
 
-# USB_PATH = Path('/media/sda1')
-USB_PATH = Path('/home/lacnguyen/PEANUT/GIT/PolliConnect-Raspberry-Pi/test_usb_parent')
+master_bp = Blueprint('master', __name__)
+
+MASTER_DATA_FOLDER = None
+
 
 # List of image extensions to look for
 image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff')
+
+
+@master_bp.record
+def record_params(setup_state):
+    global MASTER_DATA_FOLDER
+    current_app = setup_state.app
+    MASTER_DATA_FOLDER = Path(current_app.config["MASTER_DATA_FOLDER"])
 
 
 def get_connected_devices():
@@ -19,21 +26,28 @@ def get_connected_devices():
         {"serial": "30000000abcd1234", "ip": "192.168.4.3", "mac": "dc:a6:32:7b:19:f0", "hostname": "pi3", "source": "dhcp"},
     ]
     return devices
-    
 
-@app.route('/')
+
+# function to retrieve all devices connected to the Pi's network (maybe exclude non-raspberry-pi because they don't have unique ID aka serial number)
+@master_bp.route('/devices')
+def show_connected_devices():
+    devices = get_connected_devices()
+    return devices
+
+
+@master_bp.route('/')
 def hello():
     return render_template("home.html")
 
 
-@app.route('/data', methods=['GET'])
+@master_bp.route('/data', methods=['GET'])
 def fetch_data():
     try:
         # List directories in the USB
-        if not USB_PATH.exists():
+        if not MASTER_DATA_FOLDER.exists():
             return jsonify({"status": "error", "message": "USB drive not found"}), 404
         
-        device_ids = [folder.name for folder in USB_PATH.iterdir() if folder.is_dir()]
+        device_ids = [folder.name for folder in MASTER_DATA_FOLDER.iterdir() if folder.is_dir()]
 
         return render_template("data.html", device_ids=device_ids)
 
@@ -43,16 +57,9 @@ def fetch_data():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 
-# function to retrieve all devices connected to the Pi's network (maybe exclude non-raspberry-pi because they don't have unique ID aka serial number)
-@app.route('/devices')
-def show_connected_devices():
-    devices = get_connected_devices()
-    return devices
-
-
-@app.route('/data/<device_id>', methods=['GET'])
+@master_bp.route('/data/<device_id>', methods=['GET'])
 def get_device_data(device_id):
-    device_folder = USB_PATH / device_id
+    device_folder = MASTER_DATA_FOLDER / device_id
 
     if not device_folder.exists():
         return jsonify({"status": "error", "message": f"Device {device_id} not found"}), 404
@@ -71,16 +78,16 @@ def get_device_data(device_id):
     
 
 # Route to serve individual files from the device folder
-@app.route('/data/<device_id>/<filename>')
+@master_bp.route('/data/<device_id>/<filename>')
 def serve_file(device_id, filename):
-    device_folder = USB_PATH / device_id
+    device_folder = MASTER_DATA_FOLDER / device_id
     return send_from_directory(directory=device_folder, path=filename)
 
 
 # Receive files from child devices and save them in device-specific folders
-@app.route('/upload/<device_id>', methods=['POST'])
+@master_bp.route('/upload/<device_id>', methods=['POST'])
 def upload_file(device_id):
-    device_folder = USB_PATH / device_id
+    device_folder = MASTER_DATA_FOLDER / device_id
     device_folder.mkdir(parents=True, exist_ok=True)  # create folder if it doesn't exist
 
     if 'files' not in request.files:
@@ -97,6 +104,3 @@ def upload_file(device_id):
         saved_files.append(str(save_path))
 
     return jsonify({"message": f"Saved {len(saved_files)} files", "files": saved_files}), 200
-    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ parent_pi/config.py
@@ -0,0 +1,4 @@
+class Config:
+    DEBUG = True
+    MASTER_DATA_FOLDER = "/home/lacnguyen/PEANUT/GIT/PolliConnect-Raspberry-Pi/test_usb_parent" if DEBUG else "/media/sda1/"
+    DATABASE_URI = ""
